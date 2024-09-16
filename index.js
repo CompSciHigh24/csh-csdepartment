e5dbd3251d70a50024036700e198c9c00409799b
CompSciHigh24 <csh-github@compscihigh.org>
2024-09-15 22:15:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -111,6 +111,6 @@ app.use((req, res, next) => {
 	res.status(404).sendFile(__dirname + "/public/404.html")
 });
 
-app.listen(port, (req, res) => {
+app.listen(port,'0.0.0.0', (req, res) => {
 	console.log("Sever RUNNING!!!");
 });
